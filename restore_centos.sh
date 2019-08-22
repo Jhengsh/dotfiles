75d26eafaa3de9373251b3d8cacb663ed8496e7a
Jhengsh <jhengsh.email@gmail.com>
2019-08-23 00:14:52 +0800

    update restore_centos.sh and restore_debian.sh

--- restore_centos.sh
+++ restore_centos.sh
@@ -4,10 +4,10 @@ cp ./bash/.bash_profile ~/.bash_profile.local
 cp ./git/.gitconfig ~/ && cp ./git/.gitignore_global ~/
 
 # Remove alias
-sed -i -e "s/alias rm='trash'//g" ~/.bash_profile.local
+# sed -i -e "s/alias rm='trash'//g" ~/.bash_profile.local
 
 # Remove Mac pyenv Setting
-sed -i -e "s/export PYENV_ROOT/# export PYENV_ROOT/g" ~/.bash_profile.local
-sed -i -e "s/if which pyenv/# if which pyenv/g" ~/.bash_profile.local
+# sed -i -e "s/export PYENV_ROOT/# export PYENV_ROOT/g" ~/.bash_profile.local
+# sed -i -e "s/if which pyenv/# if which pyenv/g" ~/.bash_profile.local
 
 echo -e "\nsource ~/.bash_profile.local # load my config" >> ~/.bash_profile
